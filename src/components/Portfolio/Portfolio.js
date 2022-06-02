788f51495f2036d481492643b466d2b1a5244145
ChotanSharma <chotansharma@yahoo.com>
2022-06-02 02:47:11 -0400

    add portfolio page

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,10 +1,68 @@
 import React from 'react';
 import "./portfolio.css";
+import {Carousel} from "react-bootstrap";
 
 function Portfolio ()  {
     return (
-        <div className="portfolio" id='portfolio'>
-        </div>
+
+        <div class="container">
+					<header>
+						<h1>Portfolio</h1>
+						<p>I really enjoyed working in these projects. They had been amazing opportunities to hone my skills as a full stack developer.</p>
+					</header>
+					<div class="row">
+                        <h2>Here're some of my projects:</h2>
+						<Carousel variant="dark">
+                            <Carousel.Item>
+                                <img
+                                className="d-block w-80"
+                                src="assets/random-travel-generator.png"
+                                alt="First Project Slide"
+                                />
+                                <Carousel.Caption>
+                                <h5>Random Travel Generator App</h5>
+                                <h3><a href="https://8bitgaming.github.io/random-travel-generator/">Random Travel Generator</a></h3>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img
+                                className="d-block w-80"
+                                src="assets/random-travel-generator.png"
+                                alt="First Project Slide"
+                                />
+                                <Carousel.Caption>
+                                <h5>Random Travel Generator App</h5>
+                                <h3><a href="https://8bitgaming.github.io/random-travel-generator/">Random Travel Generator</a></h3>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img
+                                className="d-block w-80"
+                                src="assets/random-travel-generator.png"
+                                alt="First Project Slide"
+                                />
+                                <Carousel.Caption>
+                                <h5>Random Travel Generator App</h5>
+                                <h3><a href="https://8bitgaming.github.io/random-travel-generator/">Random Travel Generator</a></h3>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img
+                                className="d-block w-80"
+                                src="assets/random-travel-generator.png"
+                                alt="First Project Slide"
+                                />
+                                <Carousel.Caption>
+                                <h5>Random Travel Generator App</h5>
+                                <h3><a href="https://8bitgaming.github.io/random-travel-generator/">Random Travel Generator</a></h3>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                        </Carousel>
+						
+                </div>
+            </div>            
+
+        
     )
     
 }
